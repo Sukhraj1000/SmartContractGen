ac3d7e405b80012eee38c6f7519e584c2c13f919
sukhraj1000 <sukhrajkalon@gmail.com>
2025-04-10 20:52:12 +0100

    Clean up temporary files

--- recreate-history.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-while IFS=" " read -r hash author email timestamp message; do
-  GIT_COMMITTER_DATE="$timestamp" GIT_AUTHOR_DATE="$timestamp" GIT_COMMITTER_NAME="$author" GIT_AUTHOR_NAME="$author" GIT_COMMITTER_EMAIL="$email" GIT_AUTHOR_EMAIL="$email" git commit --allow-empty -m "$message"
-  echo "Recreated commit: $message"
-done < commit-history.txt
